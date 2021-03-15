a2966d2a224182c33d175b45fdb37e4211549cb1
Thiago Ribeiro <thiago.escola321@gmail.com>
2021-03-14 23:57:35 -0300

    Finalização do programa

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String palavra;
+            
+            Console.Write("Digite uma frase contendo r:");
+            palavra = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write($"{palavra.Replace("r"  , "l"  )} ");
+            
+        
+        }
+    }
+}
